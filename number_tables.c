d30af7ec28ebf9d3d24a595928736a734fec7c89
bharati0192 <99352713+bharati0192@users.noreply.github.com>
2022-07-19 22:46:20 +0530

    number_tables.c
    number_tables.c

---
+++ number_tables.c
@@ -0,0 +1,14 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+ int i,n;
+ printf("Enter the value of n\n");
+ scanf("%d",&n);
+ for(i=n;i;i--){
+     printf("%d\t",i);
+ }
+    
+    
+    return 0;
+}
